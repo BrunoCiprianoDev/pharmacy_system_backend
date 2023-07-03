806639ec4c277a49ef6b588ce564646716d8b5a9
Bruno Cipriano <cipriano990@gmail.com>
2023-07-03 18:36:10 -0300

    Insert cpf / cnpj validation

--- pharmacysystem/src/main/java/com/bcipriano/pharmacysystem/validation/CnpjValidation.java
+++ pharmacysystem/src/main/java/com/bcipriano/pharmacysystem/validation/CnpjValidation.java
@@ -15,8 +15,48 @@ public class CnpjValidation implements ConstraintValidator<Cnpj, String> {
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
-        String cnpj = value == null ? "" : value;
-        return cnpj.matches( "^\\d{2}\\.\\d{3}\\/\\d{4}\\-\\d{2}$");
+        // Remove caracteres não numéricos do CNPJ
+        String cnpj = value == null ? "" : value.replaceAll("\\D", "");
+
+        // Verifica se o CNPJ possui 14 dígitos
+        if (cnpj.length() != 14) {
+            return false;
+        }
+
+        // Verifica se todos os dígitos são iguais (ex: 00000000000000)
+        if (cnpj.matches("(\\d)\\1{13}")) {
+            return false;
+        }
+
+        // Calcula o primeiro dígito verificador
+        int sum = 0;
+        int weight = 2;
+        for (int i = 11; i >= 0; i--) {
+            int digit = Character.getNumericValue(cnpj.charAt(i));
+            sum += digit * weight;
+            weight = (weight == 9) ? 2 : weight + 1;
+        }
+        int digit1 = (sum % 11 < 2) ? 0 : 11 - (sum % 11);
+
+        // Calcula o segundo dígito verificador
+        sum = 0;
+        weight = 2;
+        for (int i = 12; i >= 0; i--) {
+            int digit = Character.getNumericValue(cnpj.charAt(i));
+            sum += digit * weight;
+            weight = (weight == 9) ? 2 : weight + 1;
+        }
+        int digit2 = (sum % 11 < 2) ? 0 : 11 - (sum % 11);
+
+        // Verifica se os dígitos verificadores estão corretos
+        return Character.getNumericValue(cnpj.charAt(12)) == digit1 && Character.getNumericValue(cnpj.charAt(13)) == digit2;
     }
 
+
 }
+
+/*
+*      String cnpj = value == null ? "" : value;
+        return cnpj.matches( "^\\d{2}\\.\\d{3}\\/\\d{4}\\-\\d{2}$");
+* */
+
--- pharmacysystem/src/main/java/com/bcipriano/pharmacysystem/validation/CpfValidation.java
+++ pharmacysystem/src/main/java/com/bcipriano/pharmacysystem/validation/CpfValidation.java
@@ -15,7 +15,44 @@ public class CpfValidation implements ConstraintValidator<Cpf, String> {
     @Override
     public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
         String cpf = value == null ? "" : value;
-        return cpf.matches( "^\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}$");
+        cpf = cpf.replaceAll("[^0-9]", ""); // Remove caracteres não numéricos do CPF
+
+        if (cpf.length() != 11) {
+            return false;
+        }
+
+        // Verifica se todos os dígitos são iguais
+        if (cpf.matches("(\\d)\\1{10}")) {
+            return false;
+        }
+
+        int[] digitos = new int[11];
+        for (int i = 0; i < 11; i++) {
+            digitos[i] = Integer.parseInt(cpf.substring(i, i + 1));
+        }
+
+        // Calcula o primeiro dígito verificador
+        int soma = 0;
+        for (int i = 0; i < 9; i++) {
+            soma += digitos[i] * (10 - i);
+        }
+        int digito1 = 11 - (soma % 11);
+        if (digito1 == 10 || digito1 == 11) {
+            digito1 = 0;
+        }
+
+        // Calcula o segundo dígito verificador
+        soma = 0;
+        for (int i = 0; i < 10; i++) {
+            soma += digitos[i] * (11 - i);
+        }
+        int digito2 = 11 - (soma % 11);
+        if (digito2 == 10 || digito2 == 11) {
+            digito2 = 0;
+        }
+
+        // Verifica se os dígitos verificadores calculados são iguais aos dígitos informados
+        return digitos[9] == digito1 && digitos[10] == digito2;
     }
 
 }
